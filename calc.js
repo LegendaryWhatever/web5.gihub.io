dadc6101ec6f4c84906009bc471b2b1ea0e8ee7b
LegendaryWhatever <112868675+LegendaryWhatever@users.noreply.github.com>
2022-10-31 20:09:34 +0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    alert("DOM готов!");
+  });
+function calculator() {
+    var price = Number(document.getElementById("price").value);
+    var quantity = Number(document.getElementById("quantity").value);
+    var result = price * quantity;
+
+    if (Number.isNaN(result)) {
+        document.getElementById("result").innerHTML = "Не удалось посчитать";
+    } else {
+        document.getElementById("result").innerHTML = result;
+    }
+}
